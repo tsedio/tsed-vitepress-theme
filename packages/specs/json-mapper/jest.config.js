6dee7c384a11c7fe1932a8c968337a3362913ab8
Xavier <xavier.jamito@zenika.com>
2024-03-13 15:04:10 +0800

    docs: run lint fix

--- packages/specs/json-mapper/jest.config.js
+++ packages/specs/json-mapper/jest.config.js
@@ -1,18 +1,18 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-module.exports = {
-  ...require("@tsed/jest-config"),
-  roots: ["<rootDir>/src", "<rootDir>/test"],
-  coverageThreshold: {
-    global: {
-      statements: 99.56,
-      branches: 97.67,
-      functions: 100,
-      lines: 99.56
-    }
-  },
-  moduleNameMapper: {
-    "@tsed/json-mapper": "<rootDir>/src/index.ts"
-  }
-};
+// module.exports = {
+//   ...require("@tsed/jest-config"),
+//   roots: ["<rootDir>/src", "<rootDir>/test"],
+//   coverageThreshold: {
+//     global: {
+//       statements: 99.56,
+//       branches: 97.67,
+//       functions: 100,
+//       lines: 99.56
+//     }
+//   },
+//   moduleNameMapper: {
+//     "@tsed/json-mapper": "<rootDir>/src/index.ts"
+//   }
+// };
