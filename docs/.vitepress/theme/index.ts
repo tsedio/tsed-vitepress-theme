f989a7598612dc619dbe362ab81fbea70c4a8c4e
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-07-15 23:08:20 +0200

    feat: add CodeSandbox molecule and storybook interaction

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -9,6 +9,7 @@ import ApiList from "./molecules/api-list/ApiList.vue";
 import GithubContributors from "./organisms/github-contributors/GithubContributors.vue";
 import LazyLoadObserver from "./directives/lazyLoadObserver";
 import Button from "./molecules/button/Button.vue";
+import CodeSandbox from "./molecules/codesandbox/CodeSandbox.vue";
 
 export default {
   extends: DefaultTheme,
@@ -21,6 +22,7 @@ export default {
     app.component("ApiList", ApiList);
     app.component("ApiAnchorQuery", ApiAnchorQuery);
     app.component("GithubContributors", GithubContributors);
+    app.component("CodeSandbox", CodeSandbox);
     // eslint-disable-next-line vue/no-reserved-component-names
     app.component("Button", Button);
     app.directive("lazyload-observer", LazyLoadObserver);
