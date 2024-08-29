4229a4f899bd8ac1f6ac913b4898cf0613be3f92
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-08-29 09:43:20 +0200

    feat(components): add ButtonBoxes

---
+++ docs/.vitepress/theme/molecules/button-boxes/ButtonBoxes.spec.ts
@@ -0,0 +1,61 @@
+import ButtonBoxes from "./ButtonBoxes.vue";
+import {render, screen} from "@testing-library/vue";
+import {userEvent} from "@testing-library/user-event";
+
+describe("ButtonBoxes", () => {
+  it("should render", async () => {
+    const items = [
+      {
+        label: "All",
+        value: ""
+      },
+      {
+        label: "Official",
+        value: "official"
+      },
+      {
+        label: "Community",
+        value: "community"
+      }
+    ];
+    let value = "";
+    const onChange = (v: any) => {
+      value = v;
+      wrapper.rerender({
+        modelValue: value,
+        choices: items,
+        "onUpdate:modelValue": onChange
+      });
+    };
+
+    const wrapper = render(ButtonBoxes, {
+      props: {
+        modelValue: value,
+        choices: items,
+        "onUpdate:modelValue": onChange
+      }
+    });
+
+    const officialButton = screen.getByRole("button", {name: "Official"});
+    const AllButton = screen.getByRole("button", {name: "All"});
+
+    expect(screen.getAllByRole("button")).toHaveLength(3);
+    expect(AllButton).toBeInTheDocument();
+    expect(AllButton).toHaveAttribute("aria-checked", "true");
+    expect(officialButton).toBeInTheDocument();
+    expect(officialButton).toHaveAttribute("aria-checked", "false");
+    expect(screen.getByRole("button", {name: "Community"})).toBeInTheDocument();
+
+    await userEvent.click(screen.getByRole("button", {name: "Official"}));
+
+    expect(value).toEqual("official");
+    expect(AllButton).toHaveAttribute("aria-checked", "false");
+    expect(officialButton).toHaveAttribute("aria-checked", "true");
+
+    // it should unselect button
+    await userEvent.click(officialButton);
+
+    expect(AllButton).toHaveAttribute("aria-checked", "true");
+    expect(officialButton).toHaveAttribute("aria-checked", "false");
+  });
+});
