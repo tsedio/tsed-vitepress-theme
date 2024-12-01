8bc3488e0356e2b4ab63340f3c8c3bd22e0d27b7
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-12-01 10:53:05 +0100

    fix(organism): fix testimonial to allow html fragment

--- packages/theme/organisms/home/HomeBeforeFeatures.vue
+++ packages/theme/organisms/home/HomeBeforeFeatures.vue
@@ -23,14 +23,13 @@ const testimonial = computed(() => {
 
   <HomeContainer v-if="testimonial && isHome">
     <main class="flex flex-col items-center space-y-8 mt-4 mb-8">
-      <h1 class="font-bold clip text-xl sm:text-3xl">
-        {{ testimonial.title }}
+      <h1 class="font-bold clip text-xl sm:text-3xl" v-html="testimonial.title">
       </h1>
       <blockquote class="text-center w-[70%] text-gray-600 dark:text-gray-400 italic -top-16 relative">
         <svg class="w-8 h-8 text-gray-400 dark:text-gray-600 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
           <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
         </svg>
-        <p>{{ testimonial.description }}</p>
+        <p v-html="testimonial.description"></p>
       </blockquote>
     </main>
   </HomeContainer>
