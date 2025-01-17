5309bc7212030ccd8b74a26bf6e5f470a5322f9c
Thanatos <thanatosstr89@gmail.com>
2025-01-17 21:04:26 +0100

    fix: Update pnpm command in terminal window

--- packages/theme/organisms/home/terminal/HomeTabTerminalPnpm.vue
+++ packages/theme/organisms/home/terminal/HomeTabTerminalPnpm.vue
@@ -2,9 +2,10 @@
   <pre
       class="shiki shiki-themes github-light github-dark vp-code"
       tabindex="0"
-  ><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">pnpx</span><span
-      style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> -p</span><span
-      style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> @tsed/cli</span><span
+  ><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">pnpm</span><span
+      style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> --package=</span><span
+      style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">@tsed/cli</span><span
+      style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> dlx</span><span
       style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> tsed</span><span
       style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> init</span><span
       style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> .</span></span></code></pre>
